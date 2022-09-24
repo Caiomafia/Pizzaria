0afc1d0ddd3ce70f6e4207c70e0850a59075ace2
Caio Mafra <gamersb200@gmail.com>
2022-09-24 12:08:13 -0300

    add timers

--- NPM-NodePackageManager/PARTE.1/TIMERS5/setTimeout.js
+++
@@ -1,5 +0,0 @@
-const timeout = 3000
-const finish = () => console.log('Done!')
-
-
-console.log(finish , timeout)
---
+++ NPM-NodePackageManager/PARTE.1/TIMERS5/setinterval.js
@@ -0,0 +1,4 @@
+const timer = 1000
+const checking = () => console.log('checking!')
+
+setInterval(checking,timer)
