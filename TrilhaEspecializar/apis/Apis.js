23e6c9d7d85dd844746770e5c74aa5c97514b33f
Caio Mafra <gamersb200@gmail.com>
2022-10-04 15:48:12 -0300

    Apis.js

--- TrilhaEspecializar/apis/Apis.js
+++ TrilhaEspecializar/apis/Apis.js
@@ -1,8 +1,11 @@
+const { json } = require('express')
 const express = require('express')
 
 const app = express()
 
 app.listen('3000')
 
-app.route('/').post((req,res)=>console.log(req.body))
+app.route('/').put((req,res) => )
 
+
+app.route('/').get((req,res) =>{res.send('res.para,e')} )
